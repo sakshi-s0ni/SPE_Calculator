e32c9c05fb10a1afdb9d5483c2d5240e639d53d1
sakshi-s0ni <sakshisoni61000@gmail.com>
2023-03-22 17:58:25 +0530

    webhook

--- src/main/java/org/calculator/Calculate.java
+++ src/main/java/org/calculator/Calculate.java
@@ -15,6 +15,7 @@ public class Calculate {
         do {
 
             System.out.println("Choose from the following operations - ");
+
             System.out.println("1. Factorial\n2. Square root\n3. Power\n4. Natural Logarithm\n" +
                     "5. Exit");
 
